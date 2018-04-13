38de177a854009464f262f84d84eb5d0bb1fbf23
filmote <simon@bloggingadeadhorse.com>
2018-04-13 21:38:52 +1000

    WIP

---
+++ music/Juno FIrst_Gameover_Sound_v1.c
@@ -0,0 +1,11 @@
+// ArduboyTones stream for file "Juno FIrst_Gameover_Sound_v1.mid" created by MIDI2TONES V1.0.0 on Fri Apr 13 21:38:17 2018
+// command line: midi2tones_64bit.exe -o2 Juno FIrst_Gameover_Sound_v1 
+//   From channel 0
+const uint16_t score[] PROGMEM = {
+// Juno FIrst
+ NOTE_C3,342, NOTE_REST,85, NOTE_CS3,85, NOTE_REST,85, NOTE_CS3,85, NOTE_REST,85, NOTE_CS3,85, NOTE_REST,85,
+ NOTE_C3,342, NOTE_REST,85, NOTE_CS3,85, NOTE_REST,85, NOTE_CS3,85, NOTE_REST,85, NOTE_CS3,85, NOTE_REST,85,
+ NOTE_C3,342,
+ TONES_END
+};
+// This score contains 70 bytes.
